02508ddea5c923af21c3c7b030e1d6193f18983f
Nik Coughlin <nrkn.com@gmail.com>
2016-11-18 13:35:52 +1300

    Add schema function on factory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 'use strict'
 
 const elementData = require( '../data/elements.json' )
+const schema = require( '../data/element.schema.json' )
 
 // ensure a fresh copy, in case the consumer modifies it
 const hinfo = () => JSON.parse( JSON.stringify( elementData ) )
+
+hinfo.schema = () => JSON.parse( JSON.stringify( schema ) )
 
 module.exports = hinfo
